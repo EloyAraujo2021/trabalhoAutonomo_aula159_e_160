c7f034cdbb4cd99f80e13d270ab06d813ac2f16f
PC <PC@DESKTOP-K4J50J6.lan>
2021-02-19 11:25:48 +0000

    Versão do Eloy

--- src/m17b_trab_autonomo_a159e160/Ponto2D.java
+++ src/m17b_trab_autonomo_a159e160/Ponto2D.java
@@ -18,6 +18,23 @@ public class Ponto2D {
         return Math.abs(Math.sqrt(Math.pow((x-a.x),2)+Math.pow((y-a.y),2)));
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+
     @Override
     public String toString() {
         return "Ponto2D{" + "x=" + x + ", y=" + y + '}';
